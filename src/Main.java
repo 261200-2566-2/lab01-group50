58e552dc6ec83b237aa55c81090b553bb965ecfc
Holo <nhuneqw@gmail.com>
2023-11-18 09:09:30 +0700

    make sorting program with sort method

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,36 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int [] a = new int[n];
+        for (int i =0 ; i<n;i++) {
+            Scanner q = new Scanner(System.in);
+            a[i] = q.nextInt();
         }
+
+        sort(a);
+
+
+        for (int i =0 ; i< n;i++){
+            System.out.println(a[i]);
+        }
+    }
+
+    public static void sort(int [] arr){
+        int n = arr.length;
+        for (int i = 0; i<n;i++){
+            for (int j =0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int t = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = t;
+                }
+
+            }
+        }
+
     }
 }
